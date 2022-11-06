fac46741014e6695722a555260c15ae6a2f257f9
Richard <richard.brown0807@gmail.com>
2022-11-06 06:02:31 -0600

    commit tree 4d4f1d6e9eba395cffea361003689d401d38b0f0

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
-  //add static methods here
+ 
+  static capitalize(str) {
+  return str.split("")[0].toUpperCase() + str.slice(1)
+ }
+
+ static sanitize(str) {
+  const regex = /[^a-zA-Z-']/g;
+  const replaced = str.replace(regex, "")
+  return replaced
+  // return str.replace(/[^A-Za-z0-9-']+/g, "");
+  }
+
+  static titleize(str) {
+  //  const badWords = ["a", "an", "but", "of", "and", "for", "at", "by", "from"]
+  //   const leftOut = str.includes(badWords) 
+    return str.map(letter => letter[0].toUpperCase()+ letter.slice(1).toLowerCase()).join(" ")
+  }
 }
+
+
+
+
